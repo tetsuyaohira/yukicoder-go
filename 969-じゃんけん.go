8e03fd7831a5f1efbddac969b8987a4edc3a9630
tetsuya ohira <tetsuya@MacBook.local>
2020-02-09 01:05:07 +0900

    first commit

---
+++ 969-じゃんけん.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    var a int;
+    fmt.Scan(&a);
+
+    if a==0 {
+        fmt.Println("Yes")
+    } else if a==4 {
+        fmt.Println("Yes")
+    } else if a==10 {
+        fmt.Println("Yes")
+    } else {
+        fmt.Println("No")
+    } 
+
+}
